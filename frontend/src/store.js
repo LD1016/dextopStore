2fca2ef7ab4487c238a18d3ebac9fc7c019b6eb4
Lance Dang <lancedang@Lances-MacBook-Pro.local>
2020-10-11 10:30:47 -0700

    create store to store reducers

---
+++ frontend/src/store.js
@@ -0,0 +1,17 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { compseWithDevTools } from 'redux-devtools-extension';
+
+const reducer = combineReducers({});
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  initialState,
+  compseWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
