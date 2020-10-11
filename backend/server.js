b108aa8b33ec66c890f640610071fbb25152a0cb
Lance Dang <lancedang@Lances-MacBook-Pro.local>
2020-10-10 22:24:12 -0700

    Create seeder, models, routes

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import products from './data/products.js';
 import colors from 'colors';
 import connectDB from './config/db.js';
+import productRouters from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -11,17 +11,10 @@ connectDB();
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('API is running...');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.send(product);
-});
+app.use('/api/products', productRouters);
 
 const PORT = process.env.PORT | 5000;
 app.listen(
